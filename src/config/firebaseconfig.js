2bbe125114b6516cf5f87847711c220fd3a43490
alaasassii <alaasassijs@gmail.com>
2023-06-05 10:24:38 +0100

    second

--- src/config/firebaseconfig.js
+++ src/config/firebaseconfig.js
@@ -7,13 +7,7 @@ import {getFirestore} from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBnat55EUFC16eYHI5JG5ifttlCcqcXiO8",
-  authDomain: "fir-1-fffe4.firebaseapp.com",
-  projectId: "fir-1-fffe4",
-  storageBucket: "fir-1-fffe4.appspot.com",
-  messagingSenderId: "358320674969",
-  appId: "1:358320674969:web:8d5d369cf55223b73c4008",
-  measurementId: "G-JX0P34LLEP"
+  
 };
 
 export const app = initializeApp(firebaseConfig);
